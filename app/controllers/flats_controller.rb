139c4207ade2783f53a5f920b02d85dd2235c420
Clement lemarie <big_m@live.fr>
2024-11-14 10:38:47 +0100

    Flats rail with tailwind

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,28 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def created
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      
+    else
+
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+30.times do
+  flat = Flat.new()
+  flat.name = Faker::Cannabis.brand
+  flat.address = Faker::Address.street_address
+  flat.description = Faker::Cannabis.cannabinoid
+  flat.price_per_night = rand(25..90)
+  flat.number_of_guests = rand(3..10)
+  flat.save
+end
